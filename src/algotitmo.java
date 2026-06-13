4b093d5c5f8f4f3ebb85bf828f59073040c34052
nghtrcom-art <nghtr.com@gmail.com>
2026-06-12 21:10:46 -0300

    código pronto

--- src/algotitmo.java
+++ src/algotitmo.java
@@ -9,17 +9,31 @@ public class algotitmo {
 
         //Declarando as variáveis
         String nome;
-        int num, i = 0;
+        int num = 0, i = 0, y;
 
 
         //Instruções ao usuário e leitura das entradas
         System.out.println("Para começar, digite o seu nome");
         nome = sc.nextLine();
 
+
         System.out.println("Olá " + nome + "! Agora digite um número:");
         num = sc.nextInt();
 
-        System.out.print("Aqui está a ordem crescente:\n");
+
+        //Estrutura que irá exibir o nome digitado n vezes
+        if(nome.length() > 6){
+            for(y = 0; y < nome.length(); y ++){
+
+                System.out.print(nome + " ");
+            }
+        }else{
+
+            System.out.println(nome);
+        }
+
+        //Estrutura de repetição que irá realizar a contagem
+        System.out.print("\nAqui está a ordem crescente:\n");
         while (i < num) {
             i++;
             System.out.print(i + " ");
@@ -28,8 +42,11 @@ public class algotitmo {
         System.out.println("\nAqui está a ordem decrescente:");
         while (num > 0) {
 
-            num--;
             System.out.print(num + " ");
+            num--;
+
         }
+
+
     }
-} // Resolver problema de lógica no segundo loop, está comendo um número
+}
